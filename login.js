a4c081957454f7065d7ccbaa131e9e2c0128d4d3
Andrey Mazépas <andreymazepas@gmail.com>
2020-11-08 18:34:28 -0300

    login

--- login.js
+++ login.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Input, Button } from "react-native-elements";
+import { Input, Button, Text, Image } from "react-native-elements";
 
 export default function Login(props) {
   return (
     <View style={styles.container}>
+      <Image
+  source={{ uri: 'https://picsum.photos/200' }}
+  style={{ width: 200, height: 200}}
+/>
+<View style={{backgroundColor: '#FFF'}}>
+  <View style={{display: 'flex'}}>
       <Input
     placeholder="Nome de usuário"
     leftIcon={{ type: 'font-awesome', name: 'user' }}
   />
   <Input
+    style={{height: 50}}
     placeholder="Senha"
     leftIcon={{ type: 'font-awesome', name: 'eye' }}
+    secureTextEntry={true}
   />
-  <Button title="Entrar" onPress={() => {props.navigation.navigate('Home')}} />
+  </View>
+  <Text style={{color: 'blue', textAlign: 'right', textDecorationStyle: 'solid', alignSelf: 'flex-end', margin: 20}}>
+    Esqueceu a senha?
+  </Text>
+  <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '80%'}}>
+    <Button  title="Login Produtor" onPress={() => {props.navigation.navigate('Home')}} />
+    <Button  title="Login Comprador" onPress={() => {props.navigation.navigate('Home')}} />
+  </View>
+  </View>
     </View>
   );
 }
@@ -23,6 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+    paddingHorizontal: 20
   },
 });
